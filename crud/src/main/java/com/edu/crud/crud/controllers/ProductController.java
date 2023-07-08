267a5f4c50df762c1e077428b0deb2c4e7123fb3
mota <thiagomotape@hotmail.com.br>
2023-07-08 01:29:59 -0300

    migrations
    criando a conexão com o DB e criando tabelas com o flyway migrations

--- crud/src/main/java/com/edu/crud/crud/controllers/ProductController.java
+++ crud/src/main/java/com/edu/crud/crud/controllers/ProductController.java
@@ -1,9 +1,16 @@
 package com.edu.crud.crud.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/product")
 public class ProductController {
+    @GetMapping
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.ok("deu ok");
+    }
 }
