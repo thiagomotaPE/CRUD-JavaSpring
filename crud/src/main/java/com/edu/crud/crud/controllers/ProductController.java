60af05f931f4e43e456a97dce735fe1e993003b9
mota <thiagomotape@hotmail.com.br>
2023-07-08 03:15:46 -0300

    update
    criando o método put(update)
    deu um erro que preciso achar

--- crud/src/main/java/com/edu/crud/crud/controllers/ProductController.java
+++ crud/src/main/java/com/edu/crud/crud/controllers/ProductController.java
@@ -1,16 +1,37 @@
 package com.edu.crud.crud.controllers;
 
+import com.edu.crud.crud.domain.product.Product;
+import com.edu.crud.crud.domain.product.ProductRepository;
+import com.edu.crud.crud.domain.product.RequestProduct;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ProductRepository repository;
     @GetMapping
     public ResponseEntity getAllProducts() {
-        return ResponseEntity.ok("deu ok");
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data) {
+        Product newProduct = new Product(data);
+        repository.save(newProduct);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
+        Product product = repository.getReferenceById(data.id());
+        product.setName(data.name());
+        product.setPrice_in_cents(data.price_in_cents());
+        return ResponseEntity.ok(product);
     }
 }
